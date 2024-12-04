44114a29f951d791245687cbeb8b1ea11fccbcc3
Ziv Caspi <zivc@microsoft.com>
2024-12-04 22:59:20 +0200

    Source generation now works correctly

--- app/Program.cs
+++ app/Program.cs
@@ -2,5 +2,6 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-//Console.WriteLine(ConstStrings.TextFile);
+// TODO: Take from the other SourceGenerator project how to actually change the output build structure in a way that works.
+Console.WriteLine(ConstStrings.TextFile);
 
--- generator/generator.cs
+++ generator/generator.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Diagnostics;
 
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.CodeAnalysis;
+
 
 namespace generator
 {
@@ -11,31 +11,49 @@ namespace generator
     {
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            System.Diagnostics.Debugger.Launch();
-            System.Diagnostics.Debugger.Break();
+            // TODO: What I really want is a way to debug the code by paying attention to the generated code.
 
             // Find all files that end with .txt
             IncrementalValuesProvider<AdditionalText> textFiles
                 = context.AdditionalTextsProvider
-                //.Where(file => file.Path.EndsWith(".txt"))
+                .Where(file => IsTextFile(file))
                 ;
 
             // Read all files
             IncrementalValuesProvider<(string Name, string Content)> namesAndContents
-                = textFiles.Select((text, cancellationToken) =>
-                (Name: Path.GetFileNameWithoutExtension(text.Path), Content: text.GetText(cancellationToken)!.ToString()))
-                ;
+                = textFiles.Select((file, cancellationToken) => ReadTextFile(file, cancellationToken));
 
             // Generate the class that holds these values
-            context.RegisterSourceOutput(namesAndContents, (spc, nameAndContent) =>
+            context.RegisterSourceOutput(namesAndContents, ProduceOutput);
+
+            context.RegisterPostInitializationOutput(context1 =>
             {
-                spc.AddSource($"ConstStrings.{nameAndContent.Name}", $@"
+                // TODO: We can add arbitrary files here
+            });
+        }
+
+        private static void ProduceOutput(SourceProductionContext context, (string Name, string Content) nameAndContent)
+        {
+            // An example for generating a diagnostic message during build
+            //var dd = new DiagnosticDescriptor("0000", "title", "bla", "category", DiagnosticSeverity.Info, isEnabledByDefault: true);
+            //context.ReportDiagnostic(Diagnostic.Create(dd, location: null, messageArgs: null));
+
+            context.AddSource($"ConstStrings.{nameAndContent.Name}.cs", $@"
 public static partial class ConstStrings
 {{
     public const string {nameAndContent.Name} = ""{nameAndContent.Content}"";
 }}");
-            });
+
         }
 
+        private static bool IsTextFile(AdditionalText file)
+        {
+            return file.Path.EndsWith(".txt", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static (string Name, string Content) ReadTextFile(AdditionalText text, CancellationToken cancellationToken)
+        {
+            return (Name: Path.GetFileNameWithoutExtension(text.Path), Content: text.GetText(cancellationToken)!.ToString());
+        }
     }
 }
